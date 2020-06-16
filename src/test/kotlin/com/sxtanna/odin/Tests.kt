29c39ee149fbfd744b9d590bdd19eb895cc2e378
Sxtanna <ranald.taylor@gmail.com>
2020-06-16 01:58:11 -0400

    removed old tests

---
+++ src/test/kotlin/com/sxtanna/odin/Tests.kt
@@ -0,0 +1,61 @@
+package com.sxtanna.odin
+
+import com.sxtanna.odin.results.None
+import com.sxtanna.odin.results.Some
+import org.junit.jupiter.api.Test
+
+object Tests
+{
+	
+	private fun readTextFromFile(name: String): String
+	{
+		return requireNotNull(ClassLoader.getSystemClassLoader().getResourceAsStream(name)?.bufferedReader()?.readText()?.replace("\r", ""))
+		{
+			"Could not read code from file: $name"
+		}
+	}
+	
+	private fun odinEvalFromFile(name: String)
+	{
+		val code = readTextFromFile(name)
+		
+		when (val result = Odin.proc(code))
+		{
+			is None ->
+			{
+				throw result.info
+			}
+			is Some ->
+			{
+				val data = Odin.pull(result.data.stack.peek() ?: Unit)
+				if (data == Unit || data == "Unit")
+				{
+					return
+				}
+				
+				println("==")
+				println(" Eval >> $data")
+				println("==")
+			}
+		}
+	}
+	
+	
+	@Test
+	internal fun `test generate random decs`()
+	{
+		odinEvalFromFile("test14.o")
+	}
+	
+	@Test
+	internal fun `test tuple types are the same`()
+	{
+		odinEvalFromFile("test15.o")
+	}
+	
+	@Test
+	internal fun `test create person`()
+	{
+		odinEvalFromFile("test5.o")
+	}
+}
